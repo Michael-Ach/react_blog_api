ca6aa0d0e0ced67da5f3c626d514bdff4225b1b4
Michael-amali <michael.acheampong@amalitech.com>
2022-03-29 15:55:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        console.log("DB connection successful");
+    })
+    .catch((err)=>{ 
+        console.log(err);
+    })
+
+
+app.use(express.json());
+app.use(cors());
+// To make the images folder public
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+	  cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name)
+		// When using postman for testing, you the code below, since we can't use both formdata and raw
+		// cb(null, "img.jpeg");
+	},
+  });
+  
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	res.status(200).json("File has been uploaded");
+});
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+
+app.listen(process.env.PORT, ()=>{
+	console.log(`Listening on port ${process.env.PORT}`)
+})
