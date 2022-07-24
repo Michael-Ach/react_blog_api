0d3bda91470e48132a77b5c2fd29a42bd29a2ca4
Michael Acheampong <michaelacheampong32@yahoo.com>
2022-07-24 17:29:21 +0000

    refactor: resolve application breaking with wrong credentials

--- routes/auth.js
+++ routes/auth.js
@@ -14,26 +14,30 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json("Network error: Something went wrong");
   }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-	try {
-	  const user = await User.findOne({ username: req.body.username });
-	  !user && res.status(400).json("Wrong credentials!");
-  
-	  const validated = await bcrypt.compare(req.body.password, user.password);
-	  !validated && res.status(400).json("Wrong credentials!");
-  
-	  const { password, ...others } = user._doc;
-	  res.status(200).json(others);
-	} catch (err) {
-	  res.status(500).json(err);
-	}
-  });
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const { password, ...others } = user._doc;
+    return res.status(200).json(others);
+  } catch (err) {
+    return res.status(500).json("Network error: Something went wrong");
+  }
+});
 
 module.exports = router;
